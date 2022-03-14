b5461210a6daf874d3517859459036a5c4490670
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2022-03-14 19:58:01 +0100

    Test EmbeddingNet and TripletNetworkTask (saving and loading .ckpt)

--- libs/SiameseNetwork.py
+++ libs/SiameseNetwork.py
@@ -1,3 +1,79 @@
 from torch.optim import SGD
 from torch import nn
 import pytorch_lightning as pl
+class EmbeddingNet(nn.Module):
+    def __init__(self):
+        super(EmbeddingNet, self).__init__()
+        self.convnet = nn.Sequential(nn.Conv2d(3,32,5),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(2, stride=2),
+                                        nn.BatchNorm2d(32),
+                                        nn.Conv2d(32,64,5),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(2, stride=2))
+
+        self.fc = nn.Sequential(
+            nn.BatchNorm1d(64 * 4 * 4),
+            nn.Linear(64 * 4 * 4, 256),
+            nn.ReLU(),
+            nn.BatchNorm1d(256),
+            nn.Linear(256,128))
+
+    def forward(self, x):
+        output = self.convnet(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
+
+class TripletNetworkTask(pl.LightningModule):
+    # lr uguale a quello del progetto vecchio
+    def __init__(self, embedding_net, lr=0.002, momentum=0.99, margin=2, num_class=3):
+        super(TripletNetworkTask, self).__init__()
+        self.save_hyperparameters()
+        self.embedding_net = embedding_net
+        self.criterion = nn.TripletMarginLoss(margin=margin)
+        self.num_class = num_class
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        return SGD(self.embedding_net.parameters(), self.hparams.lr, momentum=self.hparams.momentum)
+
+    # Lightning automatically sets the model to training for training_step and to eval for validation.
+    def training_step(self, batch, batch_idx):
+
+        I_i, I_j, I_k, *_ = batch
+
+        phi_i = self.embedding_net(I_i)
+        phi_j = self.embedding_net(I_j)
+        phi_k = self.embedding_net(I_k)
+
+
+        # calcoliamo la loss
+        loss_triplet = self.criterion(phi_i, phi_j, phi_k)
+        loss_embedd = phi_i.norm(2) + phi_i.norm(2) + phi_i.norm(2)
+
+        print(f"loss embedd {loss_embedd}")
+
+        loss = loss_triplet + 0.001 *loss_embedd
+        
+        print(f"loss {loss}")
+
+        self.log('train/loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        I_i, I_j, I_k, *_ = batch
+        phi_i = self.embedding_net(I_i)
+        phi_j = self.embedding_net(I_j)
+        phi_k = self.embedding_net(I_k)
+
+        #calcolo la loss
+        loss_triplet = self.criterion(phi_i, phi_j, phi_k)
+        
+        loss_embedd = phi_i.norm(2) + phi_i.norm(2) + phi_i.norm(2)
+        loss = loss_triplet + 0.001 * loss_embedd
+
+        self.log('valid/loss', loss)
+        return loss
