242ff5c6328dd8f78bad5cab750a44a85f232bc7
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2022-03-15 20:22:07 +0100

    general update

--- libs/util.py
+++ libs/util.py
@@ -2,8 +2,9 @@ import numpy as np
 from tqdm import tqdm
 import faiss
 import torch
+import deprecated
 
-# deprecata...
+@deprecated
 def split_into_train_and_test(dataset, train_size_perc=0.8):
     train_size = int(train_size_perc * len(dataset))
     test_size = len(dataset) - train_size
@@ -12,15 +13,7 @@ def split_into_train_and_test(dataset, train_size_perc=0.8):
 
     return dataset_train, dataset_test
 
-def extract_rgb_representations(loader):
-    """ Baseline basata su nearest neighbor RGB"""
-    representations, labels = [], []
-    for batch in tqdm(loader, total=len(loader)):
-        representations.append(batch[0].view(batch[0].shape[0],-1).numpy())
-        labels.append(batch[1])
-
-    return np.concatenate(representations), np.concatenate(labels)
-
+@deprecated
 def predict_nn(train_rep, test_rep, train_label):
     """ Funzione che permette di predire le etichette sul test set analizzando l'algoritmo NN. !pip install faiss-gpu/cpu"""
     # inizializzo l'oggetto index utilizzato x indicizzare le rappresentazioni
@@ -35,12 +28,14 @@ def predict_nn(train_rep, test_rep, train_label):
     #restituisco le etichette predette
     return train_label[indices].squeeze()
 
+# FIXME: 
 def evaluate_classification(pred_label, ground_truth):
     """ Valuto la bontà delle predizioni ottenute calcolando la distanza euclidea tra il vettore di label
         predetto e quelli di ground truth"""
     dist = np.sqrt(np.sum(np.square(pred_label-ground_truth)))
     return dist
 
+@deprecated
 def set_parameter_requires_grad(model, feature_extracting: bool):
     """Helper function that sets the `require_grad` attribute of parameter in the model to False when is used feature extracting"""
 
@@ -48,8 +43,17 @@ def set_parameter_requires_grad(model, feature_extracting: bool):
         for param in model.parameters():
             param.requires_grad = False
 
+@deprecated
+def extract_rgb_rep(loader):
+    """ Baseline basata su nearest neighbor RGB"""
+    representations, labels = [], []
+    for batch in tqdm(loader, total=len(loader)):
+        representations.append(batch[0].view(batch[0].shape[0],-1).numpy())
+        labels.append(batch[1])
 
-def extract_rep_squeezeNet(model, loader, device="cpu"):
+    return np.concatenate(representations), np.concatenate(labels)
+
+def extract_rgb_rep_model(model, loader, device="cpu"):
     """ Il modello estrae vettori di rappresentazione di 1280 unità. definisco una funzione per estrarre le rappresentazioni di dataloader di training e test """
     
     # Whenever you want to test your model you want to set it to model.eval() before which will disable dropout
